c3633f48992754ff7f47a173bd68a39377c164e9
leenaaq <v584731@gmail.com>
2025-01-10 18:00:26 +0900

    fix: Actor

---
+++ RPG_Project_17/Actor.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+class Actor {
+protected:
+	string Name;//객체 이름
+	int AttackPower;//공격력
+	int HP;//현재 체력
+	int MaxHP;//최대 체력
+
+public:
+	Actor(string Name, int Attackpower, int Hp, int MaxHp) : name(? Name)
+
+};
